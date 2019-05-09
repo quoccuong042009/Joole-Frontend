bed790d731c55e294f9e0fe213ec8c088e11de30
quoccuong042009 <quoccuong042009@yahoo.com>
2019-05-08 20:49:10 -0400

    Complete Front-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModalConfig, NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,9 @@ import { RegisterFormModalComponent } from './register-form-modal/register-form-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   entryComponents: [
     RegisterFormModalComponent
--- src/app/register-form-modal/register-form-modal.component.ts
+++ src/app/register-form-modal/register-form-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,23 +9,111 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./register-form-modal.component.css']
 })
 export class RegisterFormModalComponent implements OnInit {
-  // @Input()id: number;
-  // myForm: FormGroup;
+    IMAGE_PREFIX = './assets/Images/Users/';
 
-  constructor(
-    public activeModal: NgbActiveModal
-  ) {}
+    public imagePath;
+    imgURL: any = './assets/Images/Users/holder.png';
+    public message: string;
 
-  ngOnInit() {
-  }
+    signupForm: FormGroup;
 
-  // private createForm() {
-  //   this.myForm = this.formBuilder.group({
-  //     username: '',
-  //     password: ''
-  //   });
-  // }
-  // private submitForm() {
-  //   this.activeModal.close(this.myForm.value);
-  // }
+    invalidImage: boolean;
+    invalidUsername: boolean;
+    invalidEmail: boolean;
+    existedEmail: boolean;
+    invalidPassword: boolean;
+    invalidRepeatPassword: boolean;
+
+    constructor(
+        public modal: NgbActiveModal
+    ) {}
+
+    ngOnInit() {
+        this.signupForm = new FormGroup({
+        image: new FormControl(null, [
+            Validators.required
+        ]),
+        username: new FormControl('', [
+            Validators.required
+        ]),
+        email: new FormControl('', [
+            Validators.required,
+            Validators.email
+        ]),
+        password: new FormControl('', [
+            Validators.required,
+            Validators.minLength(8)
+        ]),
+        rpassword: new FormControl('', [
+            Validators.required,
+            Validators.minLength(8)
+        ])
+        });
+    }
+
+    preview(files) {
+        console.log(files);
+        if (files.length === 0) {
+            return;
+        }
+
+        let mimeType = files[0].type;
+        if (mimeType.match(/image\/*/) == null) {
+            this.message = 'Only images are supported.';
+            return;
+        }
+
+        let reader = new FileReader();
+        this.imagePath = files;
+        reader.readAsDataURL(files[0]);
+        reader.onload = (e) => {
+            this.imgURL = reader.result;
+        };
+    }
+
+    onSignup() {
+        this.invalidImage = false;
+        this.invalidUsername = false;
+        this.invalidEmail = false;
+        this.invalidPassword = false;
+        this.invalidRepeatPassword = false;
+        this.existedEmail = false;
+
+        if (!this.signupForm.valid) {
+            if (!this.signupForm.get('image').valid) {
+                this.invalidImage = true;
+            }
+
+            if (!this.signupForm.get('username').valid) {
+                this.invalidUsername = true;
+            }
+
+            if (!this.signupForm.get('email').valid) {
+                this.invalidEmail = true;
+            }
+
+            if (!this.signupForm.get('password').valid) {
+                this.invalidPassword = true;
+
+            }
+            if (this.signupForm.get('password').value.toString()
+            !== this.signupForm.get('rpassword').value.toString()) {
+                this.invalidRepeatPassword = true;
+            }
+        } else {
+            const image = this.imgURL;
+            const username = this.signupForm.get('username').value.toString();
+            const email = this.signupForm.get('email').value.toString();
+            const password = this.signupForm.get('password').value.toString();
+            console.log(this.imagePath);
+        // this.userService.signup(email,password,firstName,lastName,date)
+        // .subscribe(res => {
+        //   if(res.toString() === 'Created'){
+        //     this.route.navigate(['login']);
+        //   } else {
+        //     this.existedEmail = true;
+        //   }
+        // });
+        }
+    }
 }
